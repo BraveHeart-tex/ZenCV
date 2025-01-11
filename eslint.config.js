884950e626c0496314fbb0b702e6392944042158
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2025-01-11 22:44:30 +0300

    chore: setup lint-staged, eslint, prettier

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import ts from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+import js from '@eslint/js';
+import { includeIgnoreFile } from '@eslint/compat';
+import { fileURLToPath } from 'node:url';
+import { FlatCompat } from '@eslint/eslintrc';
+import path from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default ts.config(
+  includeIgnoreFile(gitignorePath),
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  prettier,
+  ...compat.extends('plugin:@typescript-eslint/recommended'),
+  ...compat.extends('next/core-web-vitals'),
+  ...compat.extends('prettier'),
+);
