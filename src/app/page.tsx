d4138e3476f29dbdfd8d9e31d623bc8f9e54fad7
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2025-01-11 23:05:23 +0300

    chore: fix file matcher on lint-staged script

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 export default function Home() {
-	return (
-		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-			<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-				<h2 className="text-4xl font-semibold tracking-tight scroll-m-20">
-					Hello World
-				</h2>
-			</main>
-		</div>
-	);
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="sm:items-start flex flex-col items-center row-start-2 gap-8">
+        <h2 className="scroll-m-20 text-4xl font-semibold tracking-tight">
+          Hello World
+        </h2>
+      </main>
+    </div>
+  );
 }
