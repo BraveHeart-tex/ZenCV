9781989346952a47aebe094fc66c8062c5d039c9
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2025-01-15 18:21:20 +0300

    fix: fix document header rendering problem

--- src/components/DocumentBuilderHeader.tsx
+++ src/components/DocumentBuilderHeader.tsx
@@ -1,10 +1,12 @@
 import EditableDocumentTitle from '@/components/EditableDocumentTitle';
+import { observer } from 'mobx-react-lite';
 
-const DocumentBuilderHeader = () => {
+const DocumentBuilderHeader = observer(() => {
   return (
     <header className="flex items-center justify-center w-full">
       <EditableDocumentTitle />
     </header>
   );
-};
+});
+
 export default DocumentBuilderHeader;
