6eec49637ac4bbae8361ca3946b3075fbc0ffec1
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2025-01-16 11:48:15 +0300

    feat: put date-month fields side by side for easier navigation

--- src/components/SectionItem.tsx
+++ src/components/SectionItem.tsx
@@ -4,6 +4,7 @@ import { documentBuilderStore } from '@/lib/documentBuilderStore';
 import SectionField from '@/components/SectionField';
 import { cn } from '@/lib/utils';
 import { INTERNAL_SECTION_TYPES } from '@/lib/constants';
+import { FIELD_TYPES } from '@/lib/schema';
 
 const SectionItem = observer(({ itemId }: { itemId: number }) => {
   const item = documentBuilderStore.getItemById(itemId)!;
@@ -13,15 +14,35 @@ const SectionItem = observer(({ itemId }: { itemId: number }) => {
     <div
       className={cn(
         'p-4 px-0',
-        item.containerType === 'collapsible' && 'border rounded-md p-2',
+        item.containerType === 'collapsible' && 'border rounded-md p-2 ',
         documentBuilderStore.getSectionById(item.sectionId)?.type ===
           INTERNAL_SECTION_TYPES.PERSONAL_DETAILS &&
           'grid grid-cols-2 gap-x-4 gap-y-6',
       )}
     >
-      {fields.map((field) => (
-        <SectionField fieldId={field.id} key={field.id} />
-      ))}
+      {fields.map((field, index) => {
+        const isDateField = field.type === FIELD_TYPES.DATE_MONTH;
+        const nextFieldIsDate =
+          fields[index + 1]?.type === FIELD_TYPES.DATE_MONTH;
+
+        if (isDateField && nextFieldIsDate) {
+          return (
+            <div
+              key={field.id}
+              className="grid w-full grid-cols-2 col-span-2 gap-4"
+            >
+              <SectionField fieldId={field.id} />
+              <SectionField fieldId={fields[index + 1].id} />
+            </div>
+          );
+        }
+
+        if (index > 0 && fields[index - 1]?.type === FIELD_TYPES.DATE_MONTH) {
+          return null;
+        }
+
+        return <SectionField fieldId={field.id} key={field.id} />;
+      })}
     </div>
   );
 });
