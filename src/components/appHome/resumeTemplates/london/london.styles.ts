8ede1722b32b3ea37411d46371f0fbb65350945d
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2025-01-25 20:02:16 +0300

    feat: add EB Garamond font for london template

--- src/components/appHome/resumeTemplates/london/london.styles.ts
+++ src/components/appHome/resumeTemplates/london/london.styles.ts
@@ -1,17 +1,49 @@
-import { StyleSheet } from '@react-pdf/renderer';
+import { StyleSheet, Font } from '@react-pdf/renderer';
 import {
   DOCUMENT_TITLE_FONT_SIZE,
   PDF_BODY_FONT_SIZE,
   SECTION_LABEL_FONT_SIZE,
 } from '@/components/appHome/resumeTemplates/resumeTemplates.constants';
 
+Font.register({
+  family: 'EB Garamond',
+  fonts: [
+    {
+      src: `/fonts/EBGaramond/EBGaramond-Regular.ttf`,
+      fontWeight: 400,
+    },
+    {
+      src: `/fonts/EBGaramond/EBGaramond-Medium.ttf`,
+      fontWeight: 500,
+    },
+    {
+      src: `/fonts/EBGaramond/EBGaramond-Bold.ttf`,
+      fontWeight: 600,
+    },
+    {
+      src: `/fonts/EBGaramond/EBGaramond-Italic.ttf`,
+      fontWeight: 400,
+      fontStyle: 'italic',
+    },
+    {
+      src: `/fonts/EBGaramond/EBGaramond-MediumItalic.ttf`,
+      fontWeight: 500,
+      fontStyle: 'italic',
+    },
+    {
+      src: `/fonts/EBGaramond/EBGaramond-BoldItalic.ttf`,
+      fontWeight: 600,
+      fontStyle: 'italic',
+    },
+  ],
+});
+
 export const londonTemplateStyles = StyleSheet.create({
   page: {
     flexDirection: 'column',
     backgroundColor: '#FFFFFF',
     padding: 20,
-    // TODO:
-    // fontFamily: 'EB Garamond',
+    fontFamily: 'EB Garamond',
   },
   documentTitle: {
     fontSize: DOCUMENT_TITLE_FONT_SIZE,
