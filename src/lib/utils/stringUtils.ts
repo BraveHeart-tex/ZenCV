3e5ed5fbd59b00edd0dff2a6a1e0cd79e63d6592
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2025-01-29 14:44:17 +0300

    refactor: add template string type signature on getting container ids for better type safety

--- src/lib/utils/stringUtils.ts
+++ src/lib/utils/stringUtils.ts
@@ -13,10 +13,14 @@ export const removeHTMLTags = (htmlString: string) => {
 export const SECTION_ID_PREFIX = 'section-' as const;
 export const ITEM_ID_PREFIX = 'item-' as const;
 
-export const getSectionContainerId = (sectionId: DEX_Section['id']): string => {
+export const getSectionContainerId = (
+  sectionId: DEX_Section['id'],
+): `${typeof SECTION_ID_PREFIX}${string}` => {
   return `${SECTION_ID_PREFIX}${sectionId.toString()}`;
 };
 
-export const getItemContainerId = (itemId: DEX_Item['id']): string => {
+export const getItemContainerId = (
+  itemId: DEX_Item['id'],
+): `${typeof ITEM_ID_PREFIX}${string}` => {
   return `${ITEM_ID_PREFIX}${itemId.toString()}`;
 };
