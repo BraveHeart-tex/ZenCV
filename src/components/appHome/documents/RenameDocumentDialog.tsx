fb5e61c8d643b64a261a0f7dc1c469bad1ee7c64
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2025-01-30 19:09:49 +0300

    refactor: refactor rename document dialog with responsive dialog for better UX

--- src/components/appHome/documents/RenameDocumentDialog.tsx
+++ src/components/appHome/documents/RenameDocumentDialog.tsx
@@ -1,30 +1,25 @@
 'use client';
 import { DEX_Document } from '@/lib/client-db/clientDbSchema';
 import { useRef, useState } from 'react';
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-} from '@/components/ui/dialog';
-import { Input } from '../../ui/input';
-import { Label } from '../../ui/label';
-import { showErrorToast, showSuccessToast } from '../../ui/sonner';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { showErrorToast, showSuccessToast } from '@/components/ui/sonner';
 import { renameDocument } from '@/lib/client-db/clientDbService';
-import { DialogDescription } from '@radix-ui/react-dialog';
-import { CornerDownLeftIcon } from 'lucide-react';
-import { Button } from '../../ui/button';
+import { Button } from '@/components/ui/button';
+import ResponsiveDialog from '@/components/ui/ResponsiveDialog';
+import { dialogFooterClassNames } from '@/lib/constants';
+
+interface RenameDocumentDialogProps {
+  isOpen: boolean;
+  onOpenChange: (isOpen: boolean) => void;
+  document: DEX_Document;
+}
 
 const RenameDocumentDialog = ({
   isOpen,
   onOpenChange,
   document,
-}: {
-  document: DEX_Document;
-  isOpen: boolean;
-  onOpenChange: (isOpen: boolean) => void;
-}) => {
+}: RenameDocumentDialogProps) => {
   const [newName, setNewName] = useState(document.title);
   const input = useRef<HTMLInputElement>(null);
 
@@ -55,49 +50,41 @@ const RenameDocumentDialog = ({
   };
 
   return (
-    <Dialog
+    <ResponsiveDialog
       open={isOpen}
-      onOpenChange={(isOpen) => {
-        if (!isOpen) {
+      onOpenChange={(open) => {
+        if (!open) {
           setNewName(document.title);
         }
-        onOpenChange(isOpen);
+        onOpenChange(open);
       }}
+      title="Rename Document"
+      description={`Enter a new name for '${document.title}'`}
     >
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Rename Document</DialogTitle>
-          <DialogDescription className="text-muted-foreground text-sm">
-            Enter a new name for &quot;{document.title}&quot;
-          </DialogDescription>
-        </DialogHeader>
-        <form onSubmit={handleRenameSubmit} className="space-y-4">
-          <div className="relative">
-            <Label htmlFor="newDocumentName" className="sr-only">
-              New Document Name
-            </Label>
-            <Input
-              id="newDocumentName"
-              className="pr-10"
-              ref={input}
-              onChange={(e) => setNewName(e.target.value)}
-              value={newName}
-              minLength={1}
-              aria-invalid={newName.length === 0 ? 'true' : 'false'}
-            />
-            <CornerDownLeftIcon className="top-1/2 right-2 bg-muted text-muted-foreground absolute p-1 -translate-y-1/2 rounded-md" />
-          </div>
-          <div className="flex items-center justify-end gap-2">
-            <DialogClose asChild>
-              <Button variant={'outline'} type="button">
-                Cancel
-              </Button>
-            </DialogClose>
-            <Button type="submit">Rename</Button>
-          </div>
-        </form>
-      </DialogContent>
-    </Dialog>
+      <form onSubmit={handleRenameSubmit} className="space-y-4">
+        <div>
+          <Label htmlFor="newDocumentName">New Document Name</Label>
+          <Input
+            id="newDocumentName"
+            ref={input}
+            onChange={(e) => setNewName(e.target.value)}
+            value={newName}
+            minLength={1}
+            aria-invalid={newName.length === 0 ? 'true' : 'false'}
+          />
+        </div>
+        <div className={dialogFooterClassNames}>
+          <Button
+            variant={'outline'}
+            type="button"
+            onClick={() => onOpenChange(false)}
+          >
+            Cancel
+          </Button>
+          <Button type="submit">Rename</Button>
+        </div>
+      </form>
+    </ResponsiveDialog>
   );
 };
 
