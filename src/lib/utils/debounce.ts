ecd378b75a1b06573762d5a2e638e593c41163e9
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2025-01-31 12:16:51 +0300

    feat: improve debounce function with type safety and JSDoc comments
    - Refined debounce function to remove 'any' type and improve type safety.
    - Added `ThisParameterType` to preserve context for methods.
    - Updated JSDoc comments for better clarity and understanding of the function.

--- src/lib/utils/debounce.ts
+++ src/lib/utils/debounce.ts
@@ -1,17 +1,26 @@
+/**
+ * Creates a debounced version of the provided function, delaying its execution
+ * until after the specified wait time has elapsed since the last call.
+ *
+ * @template T - The type of the function to debounce.
+ * @param {T} func - The function to debounce.
+ * @param {number} wait - The number of milliseconds to delay.
+ * @returns {(...args: Parameters<T>) => void} A debounced version of the provided function.
+ */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default function debounce<T extends (...args: any[]) => void>(
+export default function debounce<T extends (...args: any[]) => unknown>(
   func: T,
   wait: number,
 ): (...args: Parameters<T>) => void {
   let timeoutId: ReturnType<typeof setTimeout> | null = null;
 
-  return function (...args: Parameters<T>) {
+  return function (this: ThisParameterType<T>, ...args: Parameters<T>) {
     if (timeoutId !== null) {
       clearTimeout(timeoutId);
     }
 
     timeoutId = setTimeout(() => {
-      func(...args);
+      func.apply(this, args);
     }, wait);
   };
 }
