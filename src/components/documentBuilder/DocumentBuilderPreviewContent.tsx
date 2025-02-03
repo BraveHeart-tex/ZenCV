85ce485de60d6ee2e1dc6b27cccf4642b2e37a95
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2025-02-03 17:33:43 +0300

    fix: remove unnecessary network connection check in builder preview content

--- src/components/documentBuilder/DocumentBuilderPreviewContent.tsx
+++ src/components/documentBuilder/DocumentBuilderPreviewContent.tsx
@@ -1,6 +1,5 @@
 'use client';
 import { observer } from 'mobx-react-lite';
-import { useNetworkState } from 'react-use';
 import DocumentBuilderPdfViewer from '@/components/documentBuilder/DocumentBuilderPdfViewer';
 import LondonTemplate from '@/components/appHome/resumeTemplates/london/LondonTemplate';
 import { builderRootStore } from '@/lib/stores/documentBuilder/builderRootStore';
@@ -8,8 +7,6 @@ import { INTERNAL_TEMPLATE_TYPES } from '@/lib/stores/documentBuilder/documentBu
 import ManhattanTemplate from '../appHome/resumeTemplates/manhattan/ManhattanTemplate';
 
 const DocumentBuilderPreviewContent = observer(() => {
-  const { online, previous } = useNetworkState();
-  const userLostConnection = (!online && previous) || !online;
   const pdfTemplateData = builderRootStore.templateStore.debouncedTemplateData;
   const templateType = builderRootStore.documentStore.document?.templateType;
 
@@ -31,16 +28,7 @@ const DocumentBuilderPreviewContent = observer(() => {
 
   return (
     <div className="hide-scrollbar w-full h-full overflow-auto rounded-md">
-      {userLostConnection ? (
-        <div className="flex items-center justify-center w-full h-full">
-          <p className="text-center text-muted-foreground mx-auto max-w-[75%]">
-            Document preview is not available in offline mode. Don&apos;t worry!
-            You can still edit your resume.
-          </p>
-        </div>
-      ) : (
-        <DocumentBuilderPdfViewer>{renderTemplate()}</DocumentBuilderPdfViewer>
-      )}
+      <DocumentBuilderPdfViewer>{renderTemplate()}</DocumentBuilderPdfViewer>
     </div>
   );
 });
