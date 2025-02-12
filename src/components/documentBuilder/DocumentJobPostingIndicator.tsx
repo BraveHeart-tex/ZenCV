8cb2151180b806132a6b02c011962ac044a38082
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2025-02-12 19:50:47 +0300

    feat: add tooltip on document job posting link indicator

--- src/components/documentBuilder/DocumentJobPostingIndicator.tsx
+++ src/components/documentBuilder/DocumentJobPostingIndicator.tsx
@@ -5,6 +5,7 @@ import { Button } from '../ui/button';
 import { confirmDialogStore } from '@/lib/stores/confirmDialogStore';
 import { showErrorToast, showSuccessToast } from '../ui/sonner';
 import { runInAction } from 'mobx';
+import { Tooltip, TooltipTrigger, TooltipContent } from '../ui/tooltip';
 
 const DocumentJobPostingIndicator = observer(() => {
   if (!builderRootStore.documentStore.document?.jobPosting) return null;
@@ -30,15 +31,25 @@ const DocumentJobPostingIndicator = observer(() => {
 
   return (
     <div className="group flex items-center w-full gap-2">
-      <div className="max-w-36 md:max-w-72 bg-muted whitespace-nowrap flex items-center px-1 overflow-hidden text-xs truncate rounded-md">
-        <div className="bg-background p-1 rounded-md">
-          <BriefcaseBusinessIcon className="w-3 h-3" />
-        </div>
-        <span>
-          {builderRootStore.documentStore.document.jobPosting.jobTitle} at{' '}
-          {builderRootStore.documentStore.document.jobPosting.companyName}
-        </span>
-      </div>
+      <Tooltip>
+        <TooltipTrigger className="cursor-default">
+          <div className="max-w-36 md:max-w-72 bg-muted whitespace-nowrap flex items-center px-1 overflow-hidden text-xs truncate rounded-md">
+            <div className="bg-background p-1 rounded-md">
+              <BriefcaseBusinessIcon className="w-3 h-3" />
+            </div>
+            <span>
+              {builderRootStore.documentStore.document.jobPosting.jobTitle} at{' '}
+              {builderRootStore.documentStore.document.jobPosting.companyName}
+            </span>
+          </div>
+        </TooltipTrigger>
+        <TooltipContent>
+          <span>
+            {builderRootStore.documentStore.document.jobPosting.jobTitle} at{' '}
+            {builderRootStore.documentStore.document.jobPosting.companyName}
+          </span>
+        </TooltipContent>
+      </Tooltip>
       <Button
         className="group-hover:opacity-100 group-hover:pointer-events-auto opacity-0 pointer-events-none"
         variant="ghost"
