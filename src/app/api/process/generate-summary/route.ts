c33c5f969b62442934c03f6af8830f9e98294477
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2025-02-13 10:03:53 +0300

    fix: add missing status codes for internal server errors in generate / improve summary api routes

--- src/app/api/process/generate-summary/route.ts
+++ src/app/api/process/generate-summary/route.ts
@@ -35,11 +35,16 @@ export async function POST(req: Request) {
     });
   } catch (error) {
     console.error('generate-summary error', error);
-    return NextResponse.json({
-      success: false,
-      fieldErrors: {},
-      timestamp: Date.now(),
-      message: 'An internal error occurred while processing the request.',
-    });
+    return NextResponse.json(
+      {
+        success: false,
+        fieldErrors: {},
+        timestamp: Date.now(),
+        message: 'An internal error occurred while processing the request.',
+      },
+      {
+        status: 500,
+      },
+    );
   }
 }
--- src/app/api/process/improve-summary/route.ts
+++ src/app/api/process/improve-summary/route.ts
@@ -32,11 +32,16 @@ export async function POST(req: Request) {
     });
   } catch (error) {
     console.error('improve-summary error', error);
-    return NextResponse.json({
-      success: false,
-      fieldErrors: {},
-      timestamp: Date.now(),
-      message: 'An internal error occurred while processing the request.',
-    });
+    return NextResponse.json(
+      {
+        success: false,
+        fieldErrors: {},
+        timestamp: Date.now(),
+        message: 'An internal error occurred while processing the request.',
+      },
+      {
+        status: 500,
+      },
+    );
   }
 }
