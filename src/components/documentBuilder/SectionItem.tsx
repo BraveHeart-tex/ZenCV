02cf1078943757edc821b33a8ac831edbee367af
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2025-04-13 10:07:05 +0300

    refactor: refactor section item rendering logic for better performance

--- src/components/documentBuilder/SectionItem.tsx
+++ src/components/documentBuilder/SectionItem.tsx
@@ -1,7 +1,6 @@
 'use client';
 import { observer } from 'mobx-react-lite';
 import { CONTAINER_TYPES, DEX_Item } from '@/lib/client-db/clientDbSchema';
-import type { ReactNode } from 'react';
 import HidableFieldContainer from './HidableFieldContainer';
 import { useFieldMapper } from '@/hooks/useFieldMapper';
 import { cn } from '@/lib/utils/stringUtils';
@@ -12,42 +11,44 @@ import {
 } from '@/lib/stores/documentBuilder/documentBuilder.constants';
 import { builderRootStore } from '@/lib/stores/documentBuilder/builderRootStore';
 
-const SectionItem = observer(({ itemId }: { itemId: DEX_Item['id'] }) => {
+const ContainerElement = ({ item }: { item: DEX_Item }) => {
   const { renderFields } = useFieldMapper();
-  const item = builderRootStore.itemStore.getItemById(itemId)!;
-  const fields = builderRootStore.fieldStore.getFieldsByItemId(itemId);
 
-  if (!item) return null;
-
-  const ContainerElement = ({ children }: { children: ReactNode }) => {
-    if (item.containerType === CONTAINER_TYPES.COLLAPSIBLE) {
-      return (
-        <CollapsibleSectionItemContainer itemId={item.id}>
-          {children}
-        </CollapsibleSectionItemContainer>
-      );
-    }
-
-    return (
-      <div
-        className={cn(
-          'p-4 pt-0 px-0 grid grid-cols-2 gap-4',
-          builderRootStore.sectionStore.getSectionById(item.sectionId)?.type ===
-            INTERNAL_SECTION_TYPES.PERSONAL_DETAILS &&
-            'grid grid-cols-1 md:grid-cols-2 gap-x-4 gap-y-6',
-          fields.length === 2 && 'grid grid-cols-2 gap-4',
-        )}
-      >
-        {children}
-      </div>
-    );
-  };
+  const fields = builderRootStore.fieldStore.getFieldsByItemId(item.id);
 
   if (fields.length > MAX_VISIBLE_FIELDS) {
     return <HidableFieldContainer fields={fields} />;
   }
 
-  return <ContainerElement>{renderFields(fields)}</ContainerElement>;
+  if (item.containerType === CONTAINER_TYPES.COLLAPSIBLE) {
+    return (
+      <CollapsibleSectionItemContainer itemId={item.id}>
+        {renderFields(fields)}
+      </CollapsibleSectionItemContainer>
+    );
+  }
+
+  return (
+    <div
+      className={cn(
+        'p-4 pt-0 px-0 grid grid-cols-2 gap-4',
+        builderRootStore.sectionStore.getSectionById(item.sectionId)?.type ===
+          INTERNAL_SECTION_TYPES.PERSONAL_DETAILS &&
+          'grid grid-cols-1 md:grid-cols-2 gap-x-4 gap-y-6',
+        fields.length === 2 && 'grid grid-cols-2 gap-4',
+      )}
+    >
+      {renderFields(fields)}
+    </div>
+  );
+};
+
+const SectionItem = observer(({ itemId }: { itemId: DEX_Item['id'] }) => {
+  const item = builderRootStore.itemStore.getItemById(itemId)!;
+
+  if (!item) return null;
+
+  return <ContainerElement item={item} />;
 });
 
 export default SectionItem;
