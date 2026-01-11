a385ae812f188224032dfce37fcc1fa7d271c196
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2026-01-11 10:50:20 +0300

    feat(health-check): add authenticated health‑check endpoint verify Redis

--- src/middleware.ts
+++ src/middleware.ts
@@ -123,7 +123,6 @@ export default clerkMiddleware(async (auth, req) => {
 
 export const config = {
   matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api)(.*)',
+    '/((?!_next|api/health-check|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
   ],
 };
