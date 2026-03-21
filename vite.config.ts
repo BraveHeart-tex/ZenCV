0b56f68caaca3f05bc189d32d74ca25b487a92ea
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2026-03-21 15:25:47 +0300

    fix: restore module preloading to reduce initial bundle size

--- vite.config.ts
+++ vite.config.ts
@@ -35,7 +35,6 @@ export default defineConfig(({ mode }) => {
       },
     },
     build: {
-      modulePreload: false,
       rollupOptions: {
         output: {
           manualChunks: (id) => {
